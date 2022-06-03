edcce56328ab372fa5c1af20e2aab4ff05d5775e
unknown <ochibuike798@gmail.com>
2022-06-03 16:32:08 +0100

    this is my first commit on this project

---
+++ script.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+    $('.fa-bars').click(function () {
+        $('.nav_section').toggleClass('show')
+        
+
+    })
+    
+    
+   $('button').click(function () { 
+         
+       $('button').css({backgroundColor: 'green', color: 'red'});
+       
+       
+       
+   });
+
+})
